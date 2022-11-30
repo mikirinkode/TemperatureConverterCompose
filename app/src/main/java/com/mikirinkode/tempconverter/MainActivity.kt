f712f13f1cfc8255cc46537b6872b4e3d4b672c3
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-11-30 15:55:43 +0700

    finish project

--- app/src/main/java/com/mikirinkode/tempconverter/MainActivity.kt
+++ app/src/main/java/com/mikirinkode/tempconverter/MainActivity.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mikirinkode.tempconverter.theme.TempConverterTheme
 
 
@@ -21,17 +29,144 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
 }
 
 @Composable
-fun TempConverterApp() {
+fun TempConverterApp(modifier: Modifier = Modifier) {
+    var input by remember { mutableStateOf("") }
+    var output by remember { mutableStateOf("") }
+    Column(modifier) {
+        StatefulTemperatureInput()
+        StatelessTemperatureInput(
+            input = input,
+            output = output,
+            onValueChange = { newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            })
+        TwoWayConverter()
+    }
 
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun TempConverterAppPreview() {
     TempConverterTheme {
         TempConverterApp()
     }
+}
+
+@Composable
+fun StatefulTemperatureInput(
+    modifier: Modifier = Modifier,
+) {
+    var input by remember {
+        mutableStateOf("")
+    }
+
+    var output by remember {
+        mutableStateOf("")
+    }
+
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(id = R.string.stateful_converter),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(
+            value = input,
+            onValueChange = { newValue ->
+                input = newValue
+                output = convertToFahrenheit(newValue)
+            },
+            label = { Text(stringResource(id = R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(text = stringResource(id = R.string.temperature_fahrenheit, output))
+    }
+}
+
+@Composable
+fun StatelessTemperatureInput(
+    input: String,
+    output: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+
+    Column(modifier.padding(16.dp)) {
+        Text(text = "Stateless Converter", style = MaterialTheme.typography.h5)
+        OutlinedTextField(
+            value = input,
+            onValueChange = onValueChange,
+            label = { Text(stringResource(id = R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(text = stringResource(id = R.string.temperature_fahrenheit, output))
+    }
+}
+
+@Composable
+fun GeneralTemperatureInput(
+    scale: Scale,
+    input: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier) {
+        OutlinedTextField(
+            value = input,
+            onValueChange = onValueChange,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = {
+                Text(
+                    text = stringResource(
+                        id = R.string.enter_temperature,
+                        scale.scaleName
+                    )
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun TwoWayConverter(modifier: Modifier = Modifier) {
+    var celsius by remember { mutableStateOf("") }
+    var fahrenheit by remember { mutableStateOf("") }
+
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(id = R.string.two_way_converter),
+            style = MaterialTheme.typography.h5
+        )
+        GeneralTemperatureInput(scale = Scale.CELSIUS, input = celsius, onValueChange = { newInput ->
+            celsius = newInput
+            fahrenheit = convertToFahrenheit(newInput)
+        })
+        GeneralTemperatureInput(scale = Scale.FAHRENHEIT, input = fahrenheit, onValueChange = { newInput ->
+            fahrenheit = newInput
+            celsius = convertToCelsius(newInput)
+        })
+    }
+}
+
+fun convertToFahrenheit(celsius: String): String {
+    return celsius.toDoubleOrNull()?.let {
+        (it * 9 / 5) + 32
+    }.toString()
+}
+
+fun convertToCelsius(fahrenheit: String): String {
+    return fahrenheit.toDoubleOrNull()?.let {
+        (it - 32) * 5 / 9
+    }.toString()
+}
+
+enum class Scale(val scaleName: String) {
+    CELSIUS("Celsius"),
+    FAHRENHEIT("Fahrenheit")
 }
